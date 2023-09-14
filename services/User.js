5f78affb8a43a7e95ab5f6009ca0cb73af7c95a8
Djalmir <djalmir_miodutzki@hotmail.com>
2023-09-14 11:58:34 -0300

    subdomain api

--- services/User.js
+++ services/User.js
@@ -1,5 +1,5 @@
 // const baseUrl = 'http://192.168.100.100:3333/'
-const baseUrl = 'https://vps49327.publiccloud.com.br/api/'
+const baseUrl = 'https://api.razion.app.br/'
 const headers = () => {
 	if (!app.user)
 		return
