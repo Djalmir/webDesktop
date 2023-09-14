58b5dbdd4c45617862a2c6fca9adbc62cf5d4e4b
Djalmir <djalmir_miodutzki@hotmail.com>
2023-09-14 00:52:01 -0300

    /api/

--- services/User.js
+++ services/User.js
@@ -1,6 +1,5 @@
 // const baseUrl = 'http://192.168.100.100:3333/'
-// const baseUrl = 'https://razion-apis.onrender.com/'
-const baseUrl = 'https://vps49327.publiccloud.com.br/'
+const baseUrl = 'https://vps49327.publiccloud.com.br/api/'
 const headers = () => {
 	if (!app.user)
 		return
